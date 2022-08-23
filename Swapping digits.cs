05412fc2c5fe77d7178a77581d5ab8d1e95a39c1
jem-thanmay <86341905+jem-thanmay@users.noreply.github.com>
2022-08-23 12:58:46 +0530

    Add files via upload

---
+++ Swapping digits.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Swap
+    {
+        public static void numSwap(ref int dig1, ref int dig2)
+        {
+            int temp = 0;
+            temp = dig1;
+            dig1 = dig2;
+            dig2 = temp;
+
+        }
+        public static void Main()
+        {
+            int dig1 = 12;
+            int dig2 = 78;
+            Console.WriteLine($"Values before swap are {dig1},{dig2}");
+            numSwap(ref dig1, ref dig2);
+            Console.WriteLine($"Values after swap are {dig1},{dig2}");
+        }
+    }
+}
